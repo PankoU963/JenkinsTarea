10e2fcd06254a097fa40a77dd1524c72acedc8e3
PankoU963 <133022617+PankoU963@users.noreply.github.com>
2024-05-10 13:11:20 -0500

    Add files via upload

--- JavaWeb/demo/src/main/java/com/jenkins/demo/Controllers/IndexController.java
+++ JavaWeb/demo/src/main/java/com/jenkins/demo/Controllers/IndexController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class IndexController {
     
-    @GetMapping("/")
+    @GetMapping("/holamundo")
     public String getMethodName() {
         return "index.html";
     }
