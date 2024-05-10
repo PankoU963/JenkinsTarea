9fc20ef16245313f042cfd936cf27999159e4a42
PankoU963 <133022617+PankoU963@users.noreply.github.com>
2024-05-10 11:59:32 -0500

    Add files via upload

---
+++ JavaWeb/demo/src/main/java/com/jenkins/demo/Controllers/IndexController.java
@@ -0,0 +1,15 @@
+package com.jenkins.demo.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class IndexController {
+    
+    @GetMapping("/")
+    public String getMethodName() {
+        return "index.html";
+    }
+    
+}
